c6472b5bdfca4658dfe5d46f2fcfd803bcb34e03
Boirokk <chadzx10@gmail.com>
2017-03-17 07:22:58 -0400

    Added file name
    Added file name and fixed bug with excel

--- Esthec Biscuit Inventory.py
+++ Esthec Biscuit Inventory.py
@@ -1,56 +1,97 @@
-import os, pyautogui, time, xlrd
+import os, pyautogui, time, xlrd, sys
+import time
 pyautogui.PAUSE = 1
 pyautogui.FAILSAFE = True
 
-WORKBOOK_LOCATION = r"\\T13d-02\c$\Users\jblaner\Desktop\ESTHEC CUT OFFS.xlsx"
-ESTHEC_CUTOFF_LOCATION = "S:\Yachts\Drawings\Finished CadFiles\_CNC DATA\_ESTHEC-CUT-OFFS\BISCUIT.dwg"
+
+DATE = time.strftime("%x")
+fileDate = DATE.replace('/','_')
+
+WORKBOOK_LOCATION = r"S:\Product Development\Design Dept. Transfer\ESTHEC CUT OFFS.xlsx"
+ESTHEC_CUTOFF_LOCATION = r"S:\Yachts\Drawings\Finished CadFiles\_CNC DATA\_ESTHEC-CUT-OFFS\Master Do Not Delete.dwg"
 
 # Import information to a variable
 worksheet = xlrd.open_workbook(WORKBOOK_LOCATION)
-sheet = worksheet.sheet_by_index(0)
+sheet = worksheet.sheet_by_index(1)
 
 # Create lists for coordinates
+
 x = []
+
 y = []
 
 # add excel to list
 for index in range(sheet.nrows):
     # Check to see if the cell has a number in it
-    if type(sheet.cell_value(index, 8)) == float or type(sheet.cell_value(index, 8)) == int:
+    if type(sheet.cell_value(index, 0)) == float or type(sheet.cell_value(index, 0)) == int:
         # If the cell has a number in it the make sure its not 0
-        if sheet.cell_value(index, 8) == 0.0:
+        if sheet.cell_value(index, 0) == 0.0:
             pass
         else:
-             x.append(sheet.cell_value(index, 8))
-             y.append(sheet.cell_value(index, 10))
+             x.append(sheet.cell_value(index, 0))
+             y.append(sheet.cell_value(index, 2))
 
-movex = 300
-movey = 547
 
-os.startfile(ESTHEC_CUTOFF_LOCATION)
-time.sleep(10)
-pyautogui.hotkey('ctrl', 'a')
-pyautogui.press('delete')
 
-pyautogui.click(260, movey)
+movex = 500
+movey = 250
+count = 0
 
-for numx in range(len(x)):
-    pyautogui.typewrite('rectangle')
+try:
+    os.startfile(ESTHEC_CUTOFF_LOCATION)
+    time.sleep(15)
+
+    pyautogui.moveTo(movex,movey)
+
+except PermissionError:
+    pass
+
+try:
+    for numx in range(len(x)):
+        if count == 14:
+            count = 0
+            movex = 500
+            movey += 50
+        pyautogui.typewrite('rectangle')
+        pyautogui.press('enter')
+
+        try:
+            pyautogui.click(movex, movey)
+        except PermissionError:
+            pass
+
+        pyautogui.typewrite('d')
+
+        pyautogui.press('enter')
+        pyautogui.press('enter')
+
+        print('Drawing rectangle: ' + str(int(x[numx])) + ' X ' + str(int(y[numx])))
+
+        pyautogui.typewrite(str(x[numx]))
+        pyautogui.press('enter')
+        pyautogui.typewrite(str(y[numx]))
+        pyautogui.press('enter')
+
+        
+        try:
+            pyautogui.click(movex, movey)
+        except PermissionError:
+            pass
+
+        movex += 105
+        count +=1
+    pyautogui.hotkey('ctrl', 's')
+    time.sleep(3)
+    pyautogui.typewrite('Biscuit Cut Offs ' + fileDate)
     pyautogui.press('enter')
-    pyautogui.click(movex, movey)
-    pyautogui.typewrite('d')
-    pyautogui.press('enter')
-    movex += 10
-    pyautogui.typewrite(str(x[numx]))
-    pyautogui.press('enter')
-    pyautogui.typewrite(str(y[numx]))
-    pyautogui.press('enter')
-    pyautogui.click(movex, movey)
-    movex += 10
-pyautogui.hotkey('ctrl', 's')
-pyautogui.press('enter')
+
+except Exception as ex:
+    print(ex)
+
+
 
 print('There are', len(x), 'pieces of cut-offs.')
 print('Done...')
-input()
+
+exit = input()
 
